b067d38f6149139cb18093d14aa06180d73740e9
Dara Oladapo <daraoladapo@outlook.com>
2022-03-15 11:14:11 +0000

    luip -max up

--- upgrade.ps1
+++ upgrade.ps1
@@ -1,19 +1,25 @@
-function Get-Upgrades{
+function Get-Upgrades {
     $upgrades = az aks get-upgrades --resource-group $resourceGroupName --name $clusterName --query "controlPlaneProfile.upgrades" 
-    if($upgrades)
-    {
-        $upgradePaths= ($upgrades | findstr kubernetesVersion).Replace("kubernetesVersion", "").Replace(":", "").Replace('"','').Replace(" ", "")
+    if ($upgrades) {
+        $upgradePaths = ($upgrades | findstr kubernetesVersion).Replace("kubernetesVersion", "").Replace(":", "").Replace('"', '').Replace(" ", "")
         return $upgradePaths
     }
     return $null
 }
 $upgradePaths = Get-Upgrades
-$lastUpgradeInPath = $upgradePaths.Last()
+$lastUpgradeInPath = $upgradePaths[-1]
 Write-Host $lastUpgradeInPath
 while ($upgradePaths) {
     foreach ($upgradePath in $upgradePaths) {
-        Write-Host "Upgrading to $upgradePath"
-        az aks upgrade --resource-group $resourceGroupName --name $clusterName --kubernetes-version $upgradePath -y
+        if ($upgradePath -eq $maxVersionNumber) {
+            $upgradePaths = $null
+            return
+        }
+        else {
+       
+            Write-Host "Upgrading to $upgradePath"
+            az aks upgrade --resource-group $resourceGroupName --name $clusterName --kubernetes-version $upgradePath -y
+        }
     }
 }
 Write-Host "Upgrade complete"
